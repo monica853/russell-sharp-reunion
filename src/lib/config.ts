2371dbc5edf62883ea0af408bd53efcf45e6aad9
Monica Russell <monicarussell@MacBook-Air.local>
2026-09-13 13:29:27 -0400

    Add logo, favicon, and sharing image

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -24,8 +24,8 @@ export const FROM_EMAIL = "updates@russellsharpfamily.com";
 // without needing to check the Google Sheet themselves. Add or remove
 // emails here any time.
 export const ALERT_CC_EMAILS: string[] = [
-  // "monica@example.com",
-  // "another-organizer@example.com",
+  "monicalrussell@gmail.com",
+  "shaw0033@gmail.com",
 ];
 
 export const HOTEL = {
